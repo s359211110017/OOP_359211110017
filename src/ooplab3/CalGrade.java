14271b5e0ae9ea65fb55ec71b516d51fb8e7ffee
s359211110017 <narumon3664@gmail.com>
2017-11-27 16:39:51 +0700

    Lab3G&ForLoop

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -8,33 +8,32 @@ import java.util.Scanner;
 public class CalGrade {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print(" Input total score : ");
-        int Score = Integer.parseInt(reader.readLine());
+        int count = 0;
+        int score = 0;
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(" Enter Your Score (0-100) : ");
+            score = Integer.parseInt(reader.readLine());
 
-        String Grade;
-        if(Score >= 80){
-            Grade = "A";
-        }
-        else if(Score >= 70){
-            Grade = "B";
-        }
-        else if(Score >= 60){
-            Grade = "C";
-        }
-        else if(Score >= 50){
-            Grade = "D";
-        }
-        else{
-            Grade = "F";
-        }
+            if (score < 0 || score > 100) {
+                count++;
+                if (count == 3) {
+                    System.out.println("System Error.");
+                    break;
+                }
 
-        System.out.println(" The Grade is " + Grade);
-
-    }
+            } else {
+                if (score < 50) System.out.println("Your grade : F");
+                else if (score <= 59) System.out.println("Your grade : D");
+                else if (score <= 69) System.out.println("Your grade : C");
+                else if (score <= 79) System.out.println("Your grade : B");
+                else  System.out.println("Your grade : A");
+            }
 
 //    score 0-49 grade F
 //    score 50-59 grade D
 //    score 60-69 grade C
 //    score 70-79 grade B
 //    score 80-100 grade A
+        }
+    }
 }
