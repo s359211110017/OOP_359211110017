3941c1055faff374470b1142a9c5fb6052a64a8a
s359211110017 <narumon3664@gmail.com>
2017-12-25 16:12:11 +0700

    lab5

--- src/ooplab5/myArrayList.java
+++ src/ooplab5/myArrayList.java
@@ -1,7 +1,26 @@
 package ooplab5;
 
+import java.util.ArrayList;
+
 public class myArrayList {
     public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+        //add method
+        myList.add("BOW");
+        System.out.println(myList);
+        myList.add("AORN");
+        myList.add("NEW");
+        System.out.println(myList);
+        System.out.println(myList.size());
+        myList.add(2,"Hello");
+        System.out.println(myList);
+        myList.add(1,100);
+        System.out.println(myList);
+        myList.set(0,"Narubol");
+        System.out.println(myList);
+        myList.remove("Hello");
+        System.out.println(myList);
+        System.out.println(myList.indexOf(100));
 
     }//main
 }//class
