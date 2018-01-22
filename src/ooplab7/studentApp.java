88765330c36330883e4c5663dd731210c425db7a
s359211110017 <narumon3664@gmail.com>
2018-01-22 14:53:30 +0700

    Object

--- src/ooplab7/studentApp.java
+++ src/ooplab7/studentApp.java
@@ -1,5 +1,7 @@
 package ooplab7;
 
+import java.util.Scanner;
+
 public class studentApp {
     public static void main(String[] args) {
     //create object as student
@@ -11,15 +13,34 @@ public class studentApp {
 
     student s2 = new student("222222222222","Gril Saiyai","Maketing","MK221");
     showData(s1);
+    showData(s2);
 
+
+    student s3 = new student();
+    s3 = InputData(s3);
 
     }//main
 
-    private static void showData(student s1) {
+    private static student InputData(student s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter student in");
+        System.out.println("Enter student ID");
+        s.setId(scanner.nextLine());
+        System.out.print("Enter student name: ");
+        s.setname(scanner.nextLine());
+        System.out.print("Enter student major: ");
+        s.setmajor(scanner.nextLine());
+        System.out.print("Enter student group: ");
+        s.setgroup(scanner.nextLine());
+        return s;
+
+    }//inputData
+
+    private static void showData(student s) {
         System.out.println("student info: ");
-        System.out.println("Id: "+s1.getId());
-        System.out.println("Name: "+s1.getname());
-        System.out.println("Major: "+s1.getmajor());
-        System.out.println("Group: "+s1.getgroup());
+        System.out.println("Id: "+s.getId());
+        System.out.println("Name: "+s.getname());
+        System.out.println("Major: "+s.getmajor());
+        System.out.println("Group: "+s.getgroup());
     }
 }//class
